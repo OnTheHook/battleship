c9bcf2360cd329670c5695004f67bf80176825f0
Lawson Allick <lallick@gmail.com>
2022-12-12 13:29:10 -0500

    Work on ability for computer to make 'intelligent' moves

--- src/playerFactory.js
+++ src/playerFactory.js
@@ -14,8 +14,37 @@ function Player(gameboard) {
 
     return arr;
   };
+
+  const findHitSpots = function (board) {
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (board.hitMiss[i][j] === true) {
+          arr.push([i, j]);
+        }
+      }
+    }
+
+    return arr;
+  };
+
+  const findAdjacentOfHit = function (arr) {
+    let adjacentSpots = arr.reduce((acc, cur) => {
+      let [x, y] = cur;
+      acc.push([x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]);
+      return acc;
+    }, []);
+
+    let set = new Set(adjacentSpots.map(JSON.stringify));
+    let arr2 = Array.from(set).map(JSON.parse);
+
+    return arr2;
+  };
+
   const randomPlay = function (board) {
     let possibleSpots = findEmptySpots(board);
+    let hits = findHitSpots(board)
+    let adjacent = findAdjacentOfHit(hits)
     let spot = possibleSpots[Math.floor(Math.random() * possibleSpots.length)];
     board.receiveAttack(...spot);
   };
